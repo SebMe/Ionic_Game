2ddf011986c2fb6c7cc957767bbae46ea35a5adc
SebMe <meishes@hochschule-trier.de>
2015-09-01 09:58:38 +0200

    Script file splitted up

---
+++ www/js/PotionDefinitions.js
@@ -0,0 +1,12 @@
+// Data Structure Definitions
+var PotionOfDay = {
+    name: 'Potion Of Day',
+    image: 'PotionOfDay.png',
+    description: 'Time was between 8:00 and 20:59'
+}
+
+var PotionOfBeginning = {
+    name: 'Potion Of Beginning',
+    image: 'PotionOfBeginning.png',
+    description: 'Opened the Discovery Modus for the first time'
+}
