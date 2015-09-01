d2ca998c0b6d0c602a206ca1f32436c559ae3c20
SebMe <meishes@hochschule-trier.de>
2015-09-01 09:57:43 +0200

    Discovery Mode working

--- www/js/app.js
+++ www/js/app.js
@@ -18,36 +18,102 @@ var myApp = angular.module('starter', ['ionic'])
   });
 })
 
-
-// State Machine for the App, common way to have more than one view (home + potion list in this case)
-// See http://learn.ionicframework.com/formulas/navigation-and-routing-part-1/
-myApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/')
-
-    $stateProvider.state('state_PotionViewDisplayed', {
-        url: '/potions',
-        views: {
-            name_PotionView: {
-                templateUrl: 'potionView.html'
-            }
-        }
-    })
-
-    $stateProvider.state('state_HomeViewDisplayed', {
-        url: '/home',
-        views: {
-            name_Home: {
-                templateUrl: 'homeView.html'
-            }
-        }
-    })
-})
-
 // Solves an tab issue, that would otherwise be shown at the top of the screen
 myApp.config(function ($ionicConfigProvider) {
     $ionicConfigProvider.tabs.position('bottom');
     $ionicConfigProvider.tabs.style("standard"); //Makes them all look the same across all OS
 })
 
+// State Machine for the App, common way to have more than one view (homeView, potionView, potionDiscoveryView)
+// See http://learn.ionicframework.com/formulas/navigation-and-routing-part-1/
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
 
+    $stateProvider.state('state_HomeViewDisplayed', {
+        url: '/home',
+        views: {
+            name_home: {
+                templateUrl: 'templates/homeView.html'
+            }
+        }
+    })
+
+    $stateProvider.state('state_PotionViewDisplayed', {
+        url: '/potions',
+        views: {
+            name_potionView: {
+                templateUrl: 'templates/potionView.html'
+            }
+        }
+    })
+
+    $stateProvider.state('state_PotionDiscoveryViewDisplayed', {
+        url: '/discovery',
+        views: {
+            name_potionDiscoveryView: {
+                templateUrl: 'templates/potionDiscoveryView.html'
+            }
+        }
+    })
+})
+
+
+// Needed variables used in the controllers
+var potions = [PotionOfBeginning, PotionOfDay];
+// Variable ist nur gefüllt wenn der controller sie füllt, danach für jeden aufruf leer!! - verständnis für "globale" variablen fehlt
+var discoveredPotions = [];
+
+myApp.controller('potionViewController', function($scope){
+    $scope.$on('$ionicView.enter', function () {
+        $scope.discoveredPotions = discoveredPotions;
+    });
+})
+
+myApp.controller('homeViewController', function ($scope) {
+    $scope.$on('$ionicView.enter', function () {
+        $scope.potions = potions;
+        $scope.discoveredPotions = discoveredPotions;
+
+        // Debug purposes only
+        window.localStorage[PotionOfDay.name] = 'false';
+        window.localStorage[PotionOfBeginning.name] = 'false';
+        discoveredPotions = [];
+    });
+})
+
+myApp.controller('potionDiscoveryViewController', function ($scope, $interval) {
+    $scope.$on('$ionicView.enter', function () {
+    var newlyDiscoveredPotions = [];
+
+    // Potion of Beginning    
+    var potionOfBeginningDiscovered = window.localStorage[PotionOfBeginning.name];
+    if (potionOfBeginningDiscovered != 'true') {
+        discoveredPotions.push(PotionOfBeginning);
+        newlyDiscoveredPotions.push(PotionOfBeginning);
+        window.localStorage[PotionOfBeginning.name] = 'true';
+    }
+
+    // Potion Of Day 
+    var potionOfDayDiscovered = window.localStorage[PotionOfDay.name];
+    var currentHour = (new Date).getHours();
+    var itIsDaytime = currentHour >= 8 && currentHour <= 20;
+    if (itIsDaytime && potionOfDayDiscovered != 'true') {
+        newlyDiscoveredPotions.push(PotionOfDay);
+        discoveredPotions.push(PotionOfDay);
+        window.localStorage[PotionOfDay.name] = 'true';
+    }
+
+    // Show the newly discovered potions
+    var index = 0;
+    var showNewlyDiscoveredPotions = $interval(function (index) {
+        if (index < newlyDiscoveredPotions.length) {
+            $scope.potion = newlyDiscoveredPotions[index];
+        } else {
+            $scope.potion = null; // After the last discovered potion is shown the screen will be empty again (no potion displayed)
+            $interval.cancel(showNewlyDiscoveredPotions);
+        }
+    }, 1500);
+    });
+})
+    
 
