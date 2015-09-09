6b51632a7fe55952cff4c3cd9958eed511a95daa
SebMe <meishes@hochschule-trier.de>
2015-09-09 14:37:55 +0200

    Trader View implemented and working

--- www/js/ViewController/traderViewController.js
+++ www/js/ViewController/traderViewController.js
@@ -1,15 +1,81 @@
 myApp.controller('traderViewController', function ($scope, $cordovaSQLite, databaseFunctions) {
 
     $scope.$on('$ionicView.enter', function () {
-
-        $scope.info = 'View betreten';
+        databaseFunctions.getUser($cordovaSQLite).then(function (user) {
+            $scope.userAmountOfGold = user.AmountOfGold;
+        });
+        
     });
 
-    $scope.incGold = function () {
-        databaseFunctions.getUser($cordovaSQLite).then(function (values) {
-            values.AmountOfGold += 100;
-            $scope.info = values;
-            databaseFunctions.updateAllUserData($cordovaSQLite, values);
+    $scope.showPotionsToBuy = function () {
+        databaseFunctions.getAllExistingPotions($cordovaSQLite).then(function (allPotions) {
+            $scope.listOfPotions = allPotions;
+            $scope.action = 'buy';
+        });
+    };
+
+    $scope.showAllUserpotions = function () {
+        databaseFunctions.getUser($cordovaSQLite).then(function (user) {
+            $scope.listOfPotions = user.OwnedPotions;
+            $scope.action = 'sell';
+        });
+    };
+    
+    $scope.processPotion = function (potion) {
+        if ($scope.action == 'buy') {
+            $scope.buyPotion(potion);
+        } else {
+            $scope.sellPotion(potion);
+        };
+    }
+
+    $scope.buyPotion = function (potion) {
+        databaseFunctions.getUser($cordovaSQLite).then(function (user) {
+            // Potion retrieved from the Potions_Table has no Amount field, need to add Amount since a potion the user possesses has an Amount
+            potion.Amount = 1;
+
+            if (user.AmountOfGold - potion.Price >= 0) {
+                if (typeof user.OwnedPotions != 'undefined') { // If the user has no potions, the OwnedPotions field does not exist and is 'undefined'                 
+                    var addedPotion = false;
+                    // Check all potions the user has, if found increase amount
+                    for (var i = 0; i < user.OwnedPotions.length; i++) {
+                        if (potion.ID == user.OwnedPotions[i].ID) {                          
+                            user.OwnedPotions[i].Amount++;
+                            user.AmountOfGold -= potion.Price;
+                            addedPotion = true;
+                        };                       
+                    };
+                    // User didnt have the potion yet, add it
+                    if (addedPotion == false) {
+                        user.OwnedPotions.push(potion);
+                        user.AmountOfGold -= potion.Price;
+                    };
+                // User had no potions, OwnedPotions field was not defined
+                } else {
+                    user.OwnedPotions = [];
+                    user.OwnedPotions.push(potion);
+                    user.AmountOfGold -= potion.Price;
+                };
+                
+            };
+            $scope.userAmountOfGold = user.AmountOfGold;
+            databaseFunctions.updateAllUserData($cordovaSQLite, user);
+        });      
+    };
+
+    $scope.sellPotion = function (potion) {
+        databaseFunctions.getUser($cordovaSQLite).then(function (user) {
+            if (typeof user.OwnedPotions != 'undefined') { // If the user has no potions, the OwnedPotions field does not exist and is 'undefined'
+                for (var i = 0; i < user.OwnedPotions.length; i++) {
+                    if (potion.ID == user.OwnedPotions[i].ID && user.OwnedPotions[i].Amount > 0) {
+                        user.OwnedPotions[i].Amount--;
+                        user.AmountOfGold += potion.Price;
+                    };
+                };
+                $scope.userAmountOfGold = user.AmountOfGold;
+                $scope.listOfPotions = user.OwnedPotions;
+                databaseFunctions.updateAllUserData($cordovaSQLite, user);
+            };
         });
     };
 
